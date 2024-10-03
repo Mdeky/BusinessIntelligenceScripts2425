ed45788deb47b4ec19ee5ad0df421b9bf5d665fb
Hakim Ghanoudi <hakim.ghanoudi@student.ap.be>
2024-10-03 09:19:53 +0200

    Add create table script cleansed cities

---
+++ 03_10/Create_Table_CLEANSED_CITIES.sql
@@ -0,0 +1,11 @@
+BEGIN TRANSACTION
+GO
+--tabel aanmaken
+CREATE TABLE CLEANSED.Cities
+	(
+	--kolommen van cities met juiste types
+		[CityID] int NOT NULL,
+		[CityName] varchar(255) NOT NULL,
+		[StateProvinceID] int NOT NULL
+ 
+	) 
