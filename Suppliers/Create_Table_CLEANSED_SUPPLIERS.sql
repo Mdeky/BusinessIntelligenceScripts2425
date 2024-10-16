f63029338dff91f0a2a97cfabb76b85c19c458e6
Hakim Ghanoudi <hakim.ghanoudi@student.ap.be>
2024-10-16 15:26:08 +0200

    Fix create PK for cleansed cities

---
+++ Suppliers/Create_Table_CLEANSED_SUPPLIERS.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION
+GO
+--tabel aanmaken
+CREATE TABLE CLEANSED.Suppliers
+	(
+	--kolommen van cities met juiste types
+		[SupplierID] int NOT NULL,
+		[CityName] varchar(50) NOT NULL,
+		[SupplierCategoryID] int NOT NULL,
+		[BankAccountName] varchar(50) NOT NULL,
+		[BankAccountNumber] varchar(12) NOT NULL,
+		[BankInternationalCode] varchar(5) NOT NULL,
+		[DeliveryCityID] varchar(5) NOT NULL,
+		[DeliveryAddressLine1] varchar (20),
+		[DeliveryAddressLine2] varchar (100),
+		[DeliveryPostalCode] varchar (5)
+	) ON [PRIMARY]
