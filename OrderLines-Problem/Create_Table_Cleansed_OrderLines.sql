19418ca3d1c5b0f29d4d618aeebbae56bfb763c1
Hakim Ghanoudi <hakim.ghanoudi@student.ap.be>
2024-11-01 15:53:10 +0100

    Modified create table for orderlines

--- OrderLines-Problem/Create_Table_Cleansed_OrderLines.sql
+++ OrderLines-Problem/Create_Table_Cleansed_OrderLines.sql
@@ -1,77 +1,17 @@
 BEGIN TRANSACTION
 GO
 
-CREATE PROCEDURE [dbo].[move_orderlines_data]
-AS
-BEGIN
-    -- Tabel CLEANSED.OrderLines leegmaken (niet droppen!)
-    TRUNCATE TABLE CLEANSED.OrderLines;
+-- Tabel aanmaken voor CLEANSED.OrderLines
+CREATE TABLE CLEANSED.OrderLines (
+    OrderLineID INT NOT NULL,
+    OrderID INT NOT NULL,
+    StockItemID INT NOT NULL,
+    Description VARCHAR(255) NULL,
+    Quantity INT NOT NULL,
+    UnitPrice FLOAT NOT NULL,
+    TaxRate INT NOT NULL,
+    PickingCompletedWhen DATETIME NULL
+) ON [PRIMARY];
 
-    -- Data invoegen in CLEANSED.OrderLines, alleen records met geldige waarden
-    INSERT INTO CLEANSED.OrderLines (
-        OrderLineID,
-        OrderID,
-        StockItemID,
-        Description,
-        Quantity,
-        UnitPrice,
-        TaxRate,
-        PickingCompletedWhen
-    )
-    SELECT 
-        TRY_CAST(OrderLineID AS INT),
-        TRY_CAST(OrderID AS INT),
-        TRY_CAST(StockItemID AS INT),
-        Description,  -- Mag leeg zijn
-        TRY_CAST(Quantity AS INT),
-        TRY_CAST(UnitPrice AS FLOAT),
-        TRY_CAST(TaxRate AS INT),
-        TRY_CAST(PickingCompletedWhen AS DATETIME)
-    FROM RAW.OrderLines
-    WHERE 
-        TRY_CAST(OrderLineID AS INT) IS NOT NULL
-        AND TRY_CAST(OrderID AS INT) IS NOT NULL
-        AND TRY_CAST(StockItemID AS INT) IS NOT NULL
-        AND TRY_CAST(Quantity AS INT) IS NOT NULL
-        AND TRY_CAST(UnitPrice AS FLOAT) IS NOT NULL
-        AND TRY_CAST(TaxRate AS INT) IS NOT NULL
-        AND TRY_CAST(PickingCompletedWhen AS DATETIME) IS NOT NULL;
-
-    -- Data die niet voldoen aan de voorwaarden naar ARCHIVE verplaatsen
-    INSERT INTO ARCHIVE.OrderLines (
-        OrderLineID,
-        OrderID,
-        StockItemID,
-        Description,
-        Quantity,
-        UnitPrice,
-        TaxRate,
-        PickingCompletedWhen
-    )
-    SELECT 
-        TRY_CAST(OrderLineID AS INT),
-        TRY_CAST(OrderID AS INT),
-        TRY_CAST(StockItemID AS INT),
-        Description,  -- Mag leeg zijn
-        TRY_CAST(Quantity AS INT),
-        TRY_CAST(UnitPrice AS FLOAT),
-        TRY_CAST(TaxRate AS INT),
-        TRY_CAST(PickingCompletedWhen AS DATETIME)
-    FROM RAW.OrderLines
-    WHERE 
-        TRY_CAST(OrderLineID AS INT) IS NULL
-        OR TRY_CAST(OrderID AS INT) IS NULL
-        OR TRY_CAST(StockItemID AS INT) IS NULL
-        OR TRY_CAST(Quantity AS INT) IS NULL
-        OR TRY_CAST(UnitPrice AS FLOAT) IS NULL
-        OR TRY_CAST(TaxRate AS INT) IS NULL
-        OR TRY_CAST(PickingCompletedWhen AS DATETIME) IS NULL;
-
-END
+COMMIT;
 GO
-COMMIT
-
--- Procedure uitvoeren
-BEGIN TRANSACTION
-EXEC [dbo].[move_orderlines_data]
-COMMIT
