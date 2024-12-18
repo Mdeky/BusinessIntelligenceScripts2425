ba4265b22dabf08551fc32cc62ab861b5e44d554
Hakim Ghanoudi <hakim.ghanoudi@student.ap.be>
2024-12-18 17:55:56 +0100

    Fix dimdate load

--- Transform_StarScheme/DimDate/2-DimDate-DataLoad.sql
+++ Transform_StarScheme/DimDate/2-DimDate-DataLoad.sql
@@ -1,17 +1,9 @@
-WITH DateSeries AS (
-    SELECT CAST('2020-01-01' AS DATE) AS FullDate
-    UNION ALL
-    SELECT DATEADD(DAY, 1, FullDate)
-    FROM DateSeries
-    WHERE FullDate < '2030-12-31'
-)
 INSERT INTO dbo.DimDate (DateID, FullDate, Year, Month, Week)
-SELECT 
-    CAST(FORMAT(FullDate, 'yyyyMMdd') AS INT) AS DateID,
-    FullDate,
-    YEAR(FullDate) AS Year,
-    MONTH(FullDate) AS Month,
-    DATEPART(WEEK, FullDate) AS Week
-FROM 
-    DateSeries
-OPTION (MAXRECURSION 12000);
+SELECT DISTINCT 
+    CAST(FORMAT(OrderDate, 'yyyyMMdd') AS INT) AS DateID,
+    OrderDate AS FullDate,
+    YEAR(OrderDate) AS Year,
+    MONTH(OrderDate) AS Month,
+    DATEPART(WEEK, OrderDate) AS Week
+FROM WWI_OLTP.CLEANSED.Orders
+WHERE OrderDate IS NOT NULL;
